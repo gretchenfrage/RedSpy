3da109652e5380bc1096471aa67eadb30aec5c12
Phoenix <kahlo.phoenix@gmail.com>
2015-10-31 21:41:18 -0500

    Fixed null pointer exception

--- src/redspy/Player.java
+++ src/redspy/Player.java
@@ -3,10 +3,11 @@ package redspy;
 public class Player {
 
 	private Game game;
-	private Room room = game.getMap().getRoom(Map.RoomAccessor.MAINROOM);
+	private Room room;
 	
 	public Player(Game gameIn) {
 		game = gameIn;
+		room = game.getMap().getRoom(Map.RoomAccessor.MAINROOM);
 	}
 	
 	public Room getRoom() {
