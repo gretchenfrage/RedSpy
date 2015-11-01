e90f3929ab8faf68aff10dfe763be7cd4162dd81
Gretchenfrage <kahlo.phoenix@gmail.com>
2015-10-31 23:59:52 -0500

    Migrate string handling methods to StringHandling

--- src/redspy/GetName.java
+++ src/redspy/GetName.java
@@ -6,36 +6,4 @@ public interface GetName {
 
 	public String getName();
 	
-	public static String toCommaSeperatedString(ArrayList<GetName> a) {
-		if (a.size() == 0) {
-			return "";
-		} else if (a.size() == 1) {
-			return a.get(0).getName();
-		} else if (a.size() == 2) {
-			return a.get(0).getName() + " and " + a.get(1).getName();
-		} else {
-			String out = "";
-			for (int i = 0; i < a.size(); i++) {
-				out += a.get(i).getName();
-				if (i < a.size() - 2) {
-					out += ", ";
-				} else if (i == a.size() - 2) {
-					out += ", and ";
-				}
-			}
-			return out;
-		}
-	}
-	
-	public static String toSlashSeperatedString(ArrayList<GetName> a) {
-		String out = "";
-		for (int i = 0; i < a.size(); i++) {
-			out += a.get(i);
-			if (i < a.size() - 1) {
-				out += "/";
-			}
-		}
-		return out;
-	}
-	
 }
