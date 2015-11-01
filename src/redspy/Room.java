6b472282c7cd69b565d048c5874ef8a5491369ec
Phoenix <kahlo.phoenix@gmail.com>
2015-10-31 21:38:42 -0500

    Start describe and make listMercenaries methods

--- src/redspy/Room.java
+++ src/redspy/Room.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class Room {
 	
-	public static enum Type {
+	public static enum Status {
 		NORMAL, INTEL, ENDGAME
 	}
 
 	private String name;
 	private ArrayList<Room> exits = new ArrayList<Room>();
-	private Type type;
+	private Status type;
 	private ArrayList<Mercenary> mercenaries = new ArrayList<Mercenary>();
 	
-	public Room(String nameIn, Type typeIn) {
+	public Room(String nameIn, Status typeIn) {
 		name = nameIn;
 		type = typeIn;
 	}
 	
 	public Room(String nameIn) {
-		this(nameIn, Type.NORMAL);
+		this(nameIn, Status.NORMAL);
 	}
 	
 	public String getName() {
@@ -35,7 +35,7 @@ public class Room {
 		return (ArrayList<Room>) exits.clone();
 	}
 	
-	public Type getType() {
+	public Status getType() {
 		return type;
 	}
 	
@@ -50,6 +50,29 @@ public class Room {
 	@SuppressWarnings("unchecked")
 	public ArrayList<Mercenary> getMercenaries() {
 		return (ArrayList<Mercenary>) mercenaries.clone();
+	}
+	
+	public void describe() {
+		System.out.println("You are in " + name + " with " + listMercenaries());
+	}
+	
+	private String listMercenaries() {
+		if (mercenaries.size() == 1) {
+			return mercenaries.get(0).getName();
+		} else if (mercenaries.size() == 2) {
+			return mercenaries.get(0).getName() + " and " + mercenaries.get(1).getName();
+		} else {
+			String out = "";
+			for (int i = 0; i < mercenaries.size(); i++) {
+				out += mercenaries.get(i).getName();
+				if (i < mercenaries.size() - 2) {
+					out += ", ";
+				} else if (i == mercenaries.size() - 1) {
+					out += ", and ";
+				}
+			}
+			return out;
+		}
 	}
 	
 }
