8e0471dfcd9b5796caf897f6f384eb522c63fb96
Gretchenfrage <kahlo.phoenix@gmail.com>
2015-11-01 00:32:29 -0500

    Start takeUserTurn method, end at resulting methods shoot, talkTo, resuscitate, and goTo

--- src/redspy/Player.java
+++ src/redspy/Player.java
@@ -1,7 +1,11 @@
 package redspy;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Player {
 
+	private Scanner scanner = new Scanner(System.in);
 	private Game game;
 	private Room room;
 	
@@ -12,6 +16,76 @@ public class Player {
 	
 	public Room getRoom() {
 		return room;
+	}
+	
+	@SuppressWarnings("incomplete-switch")
+	public void takeUserTurn() {
+		System.out.println("What do you do?");
+		if (room.getExits().size() > 0) {
+			System.out.println("[go to " + StringHandling.toSlashSeperatedString(room.getExits()) + "]");
+		}
+		ArrayList<Mercenary> conscious = new ArrayList<Mercenary>();
+		ArrayList<Mercenary> unconscious = new ArrayList<Mercenary>();
+		for (Mercenary m : room.getMercenaries()) {
+			switch (m.getStatus()) {
+				case NORMAL:
+					conscious.add(m);
+					break;
+				case UNCONSCIOUS:
+					unconscious.add(m);
+					break;
+			}
+		}
+		if (conscious.size() > 0) {
+			System.out.println("[shoot " + StringHandling.toSlashSeperatedString(conscious) + "]");
+			System.out.println("[talk to " + StringHandling.toSlashSeperatedString(conscious) + "]");
+		}
+		if (unconscious.size() > 0) {
+			System.out.println("[resuscitate " + StringHandling.toSlashSeperatedString(unconscious));
+		}
+		undergoUserInput();
+	}
+	
+	private void undergoUserInput() {
+		System.out.print("> ");
+		String input = scanner.nextLine();
+		if (startsWith(input.toLowerCase(), "shoot") && input.length() >= 7) {
+			shoot(input.substring(6, input.length()));
+		} else if (startsWith(input.toLowerCase(), "talk to") && input.length() >= 9) {
+			talkTo(input.substring(8, input.length()));
+		} else if (startsWith(input.toLowerCase(), "resuscitate") && input.length() >= 11) {
+			resuscitate(input.substring(10, input.length()));
+		} else if (startsWith(input.toLowerCase(), "go to") && input.length() >= 7) {
+			goTo(input.substring(6, input.length()));
+		} else {
+			System.out.println("INVALID INPUT");
+			undergoUserInput();
+		}
+	}
+	
+	private void shoot(String target) {
+		
+	}
+	
+	private void talkTo(String target) {
+		
+	}
+	
+	private void resuscitate(String target) {
+		
+	}
+	
+	private void goTo(String target) {
+		
+	}
+	
+	private static boolean startsWith(String string, String startsWith) {
+		if (string.length() >= startsWith.length()) {
+			if (string.substring(0, startsWith.length()).equals(startsWith)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
