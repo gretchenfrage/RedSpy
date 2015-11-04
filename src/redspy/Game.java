78dda38be0d7424f47189c1b18bb54ea0f1492ea
Gretchenfrage <kahlo.phoenix@gmail.com>
2015-11-03 21:34:08 -0600

    Utilize specialized merc classes

--- src/redspy/Game.java
+++ src/redspy/Game.java
@@ -1,17 +1,27 @@
 package redspy;
 
+import redspy.mercs.Demo;
+import redspy.mercs.Engineer;
+import redspy.mercs.Heavy;
+import redspy.mercs.Medic;
+import redspy.mercs.Pyro;
+import redspy.mercs.Scout;
+import redspy.mercs.Sniper;
+import redspy.mercs.Soldier;
+import redspy.mercs.Spy;
+
 public class Game {
 	
 	private Map map = new Map();
-	private Mercenary scout = new Mercenary(map);
-	private Mercenary soldier = new Mercenary(map);
-	private Mercenary pyro = new Mercenary(map);
-	private Mercenary engineer = new Mercenary(map);
-	private Mercenary heavy = new Mercenary(map);
-	private Mercenary demo = new Mercenary(map);
-	private Mercenary medic = new Mercenary(map);
-	private Mercenary sniper = new Mercenary(map);
-	private Mercenary spy = new Mercenary(map);
+	private Mercenary scout = new Scout(map);
+	private Mercenary soldier = new Soldier(map);
+	private Mercenary pyro = new Pyro(map);
+	private Mercenary engineer = new Engineer(map);
+	private Mercenary heavy = new Heavy(map);
+	private Mercenary demo = new Demo(map);
+	private Mercenary medic = new Medic(map);
+	private Mercenary sniper = new Sniper(map);
+	private Mercenary spy = new Spy(map);
 	private Player player = new Player(this);
 
 	public static void main(String[] args) {
